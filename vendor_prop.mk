ad8da0ff3ba0eb03f9faee5ddd3e75cfed41196b
Murat Kozan <muratkozan350@gmail.com>
2020-12-19 11:16:29 +0300

    kuntao: Add some ims properties

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -119,6 +119,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
     persist.vendor.qti.telephony.vt_cam_interface=1
 
 # Media
