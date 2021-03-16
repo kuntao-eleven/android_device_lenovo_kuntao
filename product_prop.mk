b6ce6c06f78cb8263e1daaab8b0bb8f22b69627a
Murat Kozan <muratkozan350@gmail.com>
2021-03-16 10:58:11 +0300

    kuntao: Disable blur by default

--- product_prop.mk
+++ product_prop.mk
@@ -9,6 +9,10 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.io.scheduler=bfq
 
+# Disable blur by default
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.sf.disable_blurs=1
+
 # Charger
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.charger.enable_suspend=true
