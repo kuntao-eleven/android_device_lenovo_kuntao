efeeab1c9b0939ed1b56d5bec37eb0d94634895f
Murat Kozan <muratkozan350@gmail.com>
2021-03-16 11:14:14 +0300

    kuntao: Build bluetooth audio vendor variant

--- vndk/vndk-ext-libs.mk
+++ vndk/vndk-ext-libs.mk
@@ -10,6 +10,7 @@ EXTRA_VENDOR_LIBRARIES := \
     vendor.display.config@1.7 \
     vendor.display.config@1.8 \
     vendor.display.config@1.9 \
+    vendor.qti.hardware.bluetooth_audio@2.0 \
     vendor.qti.hardware.btconfigstore@1.0 \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.cryptfshw@1.0
