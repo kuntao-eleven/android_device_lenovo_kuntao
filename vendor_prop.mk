dcb8335a16adfe9ed7ebfe9941c90c133ff0f31e
Murat Kozan <muratkozan350@gmail.com>
2021-03-16 09:29:08 +0300

    kuntao: Import some fp gesture properties from s251

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -108,7 +108,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     log.tag.synaFpHal=W \
     log.tag.SynapticsLIB=W \
     log.tag.synaTransport=W \
-    persist.vendor.fp.gesture=1
+    persist.vendor.fp.gesture=1 \
+    persist.sys.fp.fingerUpCheck=0 \
+    persist.sys.fp.tplUpdYieldTime=400
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
